aeca65938fcb860da21e41dbfaf908968329411c
Enmaniac <Enmaniac@1e37b749-e30f-da40-b4df-3e99ea2a9c57>
2011-06-14 16:08:58 +0000

    Added
    git-svn-id: svn://localhost/EGE@68 1e37b749-e30f-da40-b4df-3e99ea2a9c57

---
+++ trunk/Sources/Core/Data/Node.cpp
@@ -0,0 +1,126 @@
+#include "Core/Data/Node.h"
+
+EGE_NAMESPACE
+
+//--------------------------------------------------------------------------------------------------------------------------------------------------------------
+Node::Node(Application* app, const EGEString& name, Node* parent, EGEPhysics::EComponentType componentType) : m_name(name), m_parent(parent)
+{
+  m_physics = ege_new PhysicsComponent(app, "node-physics-" + name, componentType);
+}
+//--------------------------------------------------------------------------------------------------------------------------------------------------------------
+Node::~Node()
+{
+  deleteAllChildNodes();
+
+  m_physics = NULL;
+}
+//--------------------------------------------------------------------------------------------------------------------------------------------------------------
+/*! Returns TRUE if object is valid. */
+bool Node::isValid() const
+{
+  return (NULL != m_physics) && m_physics->isValid();
+}
+//--------------------------------------------------------------------------------------------------------------------------------------------------------------
+/*! Creates child node with a given name and attaches it. */
+Node* Node::createChildNode(const EGEString& name, EGEPhysics::EComponentType componentType)
+{
+  Node* node;
+
+  // check if there is already node with the given name
+  if (NULL != childNode(name))
+  {
+    // cannot create
+    return NULL;
+  }
+
+  // create node
+  node = createChildNodeImpl(name, componentType);
+  if (NULL != node && node->isValid())
+  {
+    // add into vector
+    m_children.push_back(node);
+  }
+  else
+  {
+    // error!
+    EGE_DELETE(node);
+  }
+
+  return node;
+}
+//--------------------------------------------------------------------------------------------------------------------------------------------------------------
+/*! Deletes child node with a given name and detaches it. */
+void Node::deleteChildNode(const EGEString& name)
+{
+  // go thru all child nodes
+  for (EGEList<Node*>::iterator iter = m_children.begin(); iter != m_children.end(); ++iter)
+  {
+    Node* node = *iter;
+
+    // check if found
+    if (node->name() == name)
+    {
+      // delete node
+      EGE_DELETE(node);
+
+      // remove from vector
+      m_children.erase(iter);
+
+      // we are done
+      return;
+    }
+  }
+}
+//--------------------------------------------------------------------------------------------------------------------------------------------------------------
+/*! Returns number of child nodes. */
+u32 Node::childNodeCount() const
+{
+  return static_cast<u32>(m_children.size());
+}
+//--------------------------------------------------------------------------------------------------------------------------------------------------------------
+/*! Returns child node with a given name. Returns NULL if no such node exists. */
+Node* Node::childNode(const EGEString& name) const
+{
+  // go thru all child nodes
+  for (EGEList<Node*>::const_iterator iter = m_children.begin(); iter != m_children.end(); ++iter)
+  {
+    Node* node = *iter;
+
+    // check if found
+    if (node->name() == name)
+    {
+      // found
+      return node;
+    }
+  }
+
+  // not found
+  return NULL;
+}
+//--------------------------------------------------------------------------------------------------------------------------------------------------------------
+/*! Returns child node with a given index. Returns NULL if no such node exists. */
+Node* Node::childNode(u32 index) const
+{
+  // check if OUT of range
+  if (index >= childNodeCount())
+  {
+    // error!
+    return NULL;
+  }
+
+  return m_children[index];
+}
+//--------------------------------------------------------------------------------------------------------------------------------------------------------------
+/*! Deletes and detaches all child nodes. */
+void Node::deleteAllChildNodes()
+{
+  for (EGEList<Node*>::iterator iter = m_children.begin(); iter != m_children.end();)
+  {
+    Node* node = *iter;
+
+    EGE_DELETE(node);
+
+    iter = m_children.erase(iter);
+  }
+}
+//--------------------------------------------------------------------------------------------------------------------------------------------------------------
---
+++ trunk/Sources/Core/Data/Node.h
@@ -0,0 +1,86 @@
+#ifndef EGE_CORE_NODE_H
+#define EGE_CORE_NODE_H
+
+/** Class representing a general-purpose node an articulated scene graph.
+    @remarks
+        A node in the scene graph is a node in a structured tree. A node contains
+        information about the transformation which will apply to
+        it and all of it's children. Child nodes can have transforms of their own, which
+        are combined with their parent's transformations.
+    @par
+        This is an abstract class - concrete classes are based on this for specific purposes,
+        e.g. SceneNode, Bone
+*/
+
+#include <EGE.h>
+#include <EGEList.h>
+#include "Core/Math/Matrix4.h"
+#include "Core/Components/Physics/PhysicsComponent.h"
+
+EGE_NAMESPACE_BEGIN
+
+//--------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+class Renderer;
+class Application;
+
+//--------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+class Node
+{
+  public:
+
+    Node(Application* app, const EGEString& name, Node* parent, EGEPhysics::EComponentType componentType = EGEPhysics::COMPONENT_DYNAMIC);
+    virtual ~Node();
+
+    /* Returns TRUE if object is valid. */
+    bool isValid() const;
+    /*! Returns node name. */
+    inline const EGEString& name() const { return m_name; }
+    /*! Returns node's parent. NULL if this is root node. */
+    inline Node* parent() const { return m_parent; }
+
+    /* Deletes child node with a given name and detaches it. */
+    void deleteChildNode(const EGEString& name);
+    /* Deletes and detaches all child nodes. */
+    void deleteAllChildNodes();
+    /* Returns number of child nodes. */
+    u32 childNodeCount() const;
+    /* Returns child node with a given name. Returns NULL if no such node exists. */
+    Node* childNode(const EGEString& name) const;
+    /* Returns child node with a given index. Returns NULL if no such node exists. */
+    Node* childNode(u32 index) const;
+
+    /*! Returns local physics component. */
+    inline PPhysicsComponent physics() { return m_physics; }
+    /*! Returns cached combined world matrix. */
+    inline const Matrix4f& worldMatrix() const { return m_worldMatrix; }
+
+  protected:
+
+    /* Creates child node with a given name and attaches it. */
+    Node* createChildNode(const EGEString& name, EGEPhysics::EComponentType componentType);
+    /*! Creates child node with a given name. MUST be overriden by subclass. */
+    virtual Node* createChildNodeImpl(const EGEString& name, EGEPhysics::EComponentType componentType) = 0;
+
+  protected:
+
+    /*! Name. */
+    EGEString m_name;
+    /*! Raw pointer to parent node. NULL if this is root node. Pointer is RAW as this object is not intended (TAGE for now ?) to be shared. */
+    Node* m_parent;
+    /*! List of all child nodes attached. */
+    EGEList<Node*> m_children;
+    /*! Physics component. */
+    PPhysicsComponent m_physics;
+    /*! Cached combined world matrix from all self and all parent nodes. */
+    Matrix4f m_worldMatrix;
+    /*! Visibility flag. */
+    bool m_visible;
+};
+
+//--------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+EGE_NAMESPACE_END
+
+#endif // EGE_CORE_SCENENODE_H
