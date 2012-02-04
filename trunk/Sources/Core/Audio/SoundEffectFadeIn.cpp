87ff876987b40896519efe32b719f507e9d66140
Enmaniac <Enmaniac@1e37b749-e30f-da40-b4df-3e99ea2a9c57>
2012-02-04 10:38:51 +0000

    Added
    git-svn-id: svn://localhost/EGE@734 1e37b749-e30f-da40-b4df-3e99ea2a9c57

---
+++ trunk/Sources/Core/Audio/SoundEffectFadeIn.cpp
@@ -0,0 +1,64 @@
+#include "Core/Audio/SoundEffectFadeIn.h"
+#include "Core/Audio/Sound.h"
+
+EGE_NAMESPACE
+
+//--------------------------------------------------------------------------------------------------------------------------------------------------------------
+EGE_DEFINE_NEW_OPERATORS(SoundEffectFadeIn)
+EGE_DEFINE_DELETE_OPERATORS(SoundEffectFadeIn)
+//--------------------------------------------------------------------------------------------------------------------------------------------------------------
+SoundEffectFadeIn::SoundEffectFadeIn(const Time& duration) : SoundEffect(),
+                                                             m_duration(duration),
+                                                             m_time(0.0f)
+{
+}
+//--------------------------------------------------------------------------------------------------------------------------------------------------------------
+SoundEffectFadeIn::~SoundEffectFadeIn()
+{
+}
+//--------------------------------------------------------------------------------------------------------------------------------------------------------------
+/*! SoundEffect override. Returns TRUE if object is valid. */
+bool SoundEffectFadeIn::isValid() const
+{
+  return true;
+}
+//--------------------------------------------------------------------------------------------------------------------------------------------------------------
+/*! SoundEffect override. Updates object. 
+ * @param time  Time frame for which update should be done.
+ * @param sound Sound object for which effect is being run.
+ * @return  Returns TRUE if effect came to completion. Otherwise, returns FALSE.
+ */
+bool SoundEffectFadeIn::update(const Time& time, Sound* sound)
+{
+  bool done = false;
+
+  // update time
+  m_time += time;
+
+  // check if done
+  if (m_time >= m_duration)
+  {
+    // we r done
+    m_time = m_duration;
+
+    done = true;
+  }
+
+  // update volume
+  sound->setVolume(m_time.seconds() / m_duration.seconds());
+
+  if (done)
+  {
+    // notify
+    emit finished(this);
+  }
+
+  return done;
+}
+//--------------------------------------------------------------------------------------------------------------------------------------------------------------
+/*! Sets fade duration. */
+void SoundEffectFadeIn::setDuration(const Time& time)
+{
+  m_duration = time;
+}
+//--------------------------------------------------------------------------------------------------------------------------------------------------------------
