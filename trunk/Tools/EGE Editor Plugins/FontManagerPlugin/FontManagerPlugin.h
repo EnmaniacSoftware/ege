97e8801f979d0ebdeb53aff74f11915c1890048e
Enmaniac <Enmaniac@1e37b749-e30f-da40-b4df-3e99ea2a9c57>
2012-04-02 22:02:31 +0000

    Font manager plugin base
    git-svn-id: svn://localhost/EGE@857 1e37b749-e30f-da40-b4df-3e99ea2a9c57

---
+++ trunk/Tools/EGE Editor Plugins/FontManagerPlugin/FontManagerPlugin.h
@@ -0,0 +1,25 @@
+#ifndef FONTMANAGERPLUGIN_H
+#define FONTMANAGERPLUGIN_H
+
+#include <Plugins/IPlugin.h>
+#include "fontmanagerplugin_global.h"
+
+//--------------------------------------------------------------------------------------------------------------------------------------------------------------
+class FONTMANAGERPLUGIN_API FontManagerPlugin : public QObject, public IPlugin
+{
+  Q_OBJECT
+  Q_INTERFACES(IPlugin);
+
+  public:
+
+    explicit FontManagerPlugin(QObject* parent = NULL);
+    ~FontManagerPlugin();
+
+    /* IPlugin override. Initialized plugin. */
+    bool initialize() override;
+    /* IPlugin override. Deinitializes plugin. */
+    void deinitialize() override;
+};
+//--------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+#endif // FONTMANAGERPLUGIN_H
