3da8bb8f7a584ee88e8e8c6850149afc23d2d2dd
enmaniac <enmaniac@1e37b749-e30f-da40-b4df-3e99ea2a9c57>
2013-03-18 16:45:59 +0000

    IOS implementation introduction changes
    git-svn-id: svn://localhost/EGE@1482 1e37b749-e30f-da40-b4df-3e99ea2a9c57

---
+++ trunk/Sources/iOS/NativeUI/MessageBoxIOS_p.h
@@ -0,0 +1,36 @@
+#ifndef EGE_IOS_NATIVEUI_MESSAGEBOX_PRIVATE_H
+#define EGE_IOS_NATIVEUI_MESSAGEBOX_PRIVATE_H
+
+#include <EGE.h>
+#include <EGEString.h>
+
+EGE_NAMESPACE_BEGIN
+
+//--------------------------------------------------------------------------------------------------------------------------------------------------------------
+class MessageBox;
+//--------------------------------------------------------------------------------------------------------------------------------------------------------------
+/*! @brief Private implementation for native message box for iOS. */
+class MessageBoxPrivate
+{
+  public: 
+
+    MessageBoxPrivate(MessageBox* base);
+   ~MessageBoxPrivate();
+    
+    EGE_DECLARE_NEW_OPERATORS
+    EGE_DECLARE_DELETE_OPERATORS
+
+    EGE_DECLARE_PUBLIC_IMPLEMENTATION(MessageBox)
+
+  public:
+
+    /*! Constructs object. */
+    EGEResult construct();
+    /*! Shows message box. */
+    s32 show();
+};
+//--------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+EGE_NAMESPACE_END
+
+#endif // EGE_IOS_NATIVEUI_MESSAGEBOX_PRIVATE_H
