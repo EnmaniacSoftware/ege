4ad2e207d5d0c214eac2f2b1d0ec1990bbd5ffd2
enmaniac <enmaniac@1e37b749-e30f-da40-b4df-3e99ea2a9c57>
2013-03-18 15:43:26 +0000

    OpenGL refactoring
    git-svn-id: svn://localhost/EGE@1480 1e37b749-e30f-da40-b4df-3e99ea2a9c57

---
+++ trunk/Sources/iOS/Graphics/OpenGL/ExtensionsOGLIOS.h
@@ -0,0 +1,185 @@
+﻿#ifndef EGE_AIRPLAY_GRAPHICS_OPENGL_EXTENSIONSOGL_H
+#define EGE_AIRPLAY_GRAPHICS_OPENGL_EXTENSIONSOGL_H
+
+#include "EGE.h"
+#include "EGEOpenGL.h"
+
+//--------------------------------------------------------------------------------------------------------------------------------------------------------------
+#define GL_APIENTRYP *
+//--------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// Stub for unsupported features in OGLES
+#define GL_TEXTURE_1D         0x0DE0
+#define GL_TEXTURE_BINDING_1D 0x8068
+
+// GL_EXT_texture_compression_s3tc
+#define GL_COMPRESSED_RGB_S3TC_DXT1   0x83F0
+#define GL_COMPRESSED_RGBA_S3TC_DXT1  0x83F1
+#define GL_COMPRESSED_RGBA_S3TC_DXT3  0x83F2
+#define GL_COMPRESSED_RGBA_S3TC_DXT5  0x83F3
+
+//--------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+typedef const GLubyte* (GL_APIENTRYP PFNGLGETSTRINGIPROC) (GLenum name, GLuint index);
+extern PFNGLGETSTRINGIPROC glGetStringi;
+
+// GL_ARB_point_sprite
+#define GL_POINT_SPRITE   0x8861
+#define GL_COORD_REPLACE  0x8862
+
+// GL_EXT_framebuffer_object
+#define GL_FRAMEBUFFER_BINDING  0x8CA6
+#define GL_FRAMEBUFFER_COMPLETE 0x8CD5
+#define GL_COLOR_ATTACHMENT0    0x8CE0
+#define GL_FRAMEBUFFER          0x8D40
+
+typedef void (GL_APIENTRYP PFNGLGENFRAMEBUFFERSPROC) (GLsizei n, GLuint *framebuffers);
+typedef void (GL_APIENTRYP PFNGLDELETEFRAMEBUFFERSPROC) (GLsizei n, const GLuint *framebuffers);
+typedef void (GL_APIENTRYP PFNGLBINDFRAMEBUFFERPROC) (GLenum target, GLuint framebuffer);
+typedef GLenum (GL_APIENTRYP PFNGLCHECKFRAMEBUFFERSTATUSPROC) (GLenum target);
+typedef void (GL_APIENTRYP PFNGLFRAMEBUFFERTEXTURE2DPROC) (GLenum target, GLenum attachment, GLenum textarget, GLuint texture, GLint level);
+
+extern PFNGLGENFRAMEBUFFERSPROC glGenFramebuffers;
+extern PFNGLDELETEFRAMEBUFFERSPROC glDeleteFramebuffers;
+extern PFNGLBINDFRAMEBUFFERPROC glBindFramebuffer;
+extern PFNGLCHECKFRAMEBUFFERSTATUSPROC glCheckFramebufferStatus;
+extern PFNGLFRAMEBUFFERTEXTURE2DPROC glFramebufferTexture2D;
+
+// GL_OES_mapbuffer
+typedef GLvoid* (GL_APIENTRYP PFNGLMAPBUFFERPROC) (GLenum target, GLenum access);
+typedef GLboolean (GL_APIENTRYP PFNGLUNMAPBUFFERPROC) (GLenum target);
+
+extern PFNGLMAPBUFFERPROC glMapBuffer;
+extern PFNGLUNMAPBUFFERPROC glUnmapBuffer;
+
+// blend equations
+typedef void (GL_APIENTRYP PFNGLBLENDEQUATIONPROC) (GLenum mode);
+typedef void (GL_APIENTRYP PFNGLBLENDFUNCSEPARATEPROC) (GLenum sfactorRGB, GLenum dfactorRGB, GLenum sfactorAlpha, GLenum dfactorAlpha);
+
+extern PFNGLBLENDEQUATIONPROC glBlendEquation;
+extern PFNGLBLENDFUNCSEPARATEPROC glBlendFuncSeparate;
+
+// texture compression PVRTC
+#define GL_COMPRESSED_RGB_PVRTC_4BPPV1_IMG			0x8C00
+#define GL_COMPRESSED_RGB_PVRTC_2BPPV1_IMG			0x8C01
+#define GL_COMPRESSED_RGBA_PVRTC_4BPPV1_IMG			0x8C02
+#define GL_COMPRESSED_RGBA_PVRTC_2BPPV1_IMG			0x8C03
+
+// GL_ARB_shader_objects
+#define GL_OBJECT_COMPILE_STATUS  0x8B81
+#define GL_OBJECT_LINK_STATUS     0x8B82
+#define GL_OBJECT_INFO_LOG_LENGTH 0x8B84
+#define GL_OBJECT_ACTIVE_UNIFORMS 0x8B86
+
+typedef char GLchar;
+typedef unsigned int GLhandle;
+
+typedef GLhandle (GL_APIENTRYP PFNGLCREATESHADEROBJECTARBPROC) (GLenum shaderType);
+typedef void (GL_APIENTRYP PFNGLDELETEOBJECTARBPROC) (GLhandle obj);
+typedef void (GL_APIENTRYP PFNGLDETACHOBJECTARBPROC) (GLhandle containerObj, GLhandle attachedObj);
+typedef void (GL_APIENTRYP PFNGLSHADERSOURCEARBPROC) (GLhandle shaderObj, GLsizei count, const GLchar* *string, const GLint *length);
+typedef void (GL_APIENTRYP PFNGLCOMPILESHADERARBPROC) (GLhandle shaderObj);
+typedef GLhandle (GL_APIENTRYP PFNGLCREATEPROGRAMOBJECTARBPROC) (void);
+typedef void (GL_APIENTRYP PFNGLGETPROGRAMIVPROC) (GLuint program, GLenum pname, GLint *params);
+typedef void (GL_APIENTRYP PFNGLATTACHOBJECTARBPROC) (GLhandle containerObj, GLhandle obj);
+typedef void (GL_APIENTRYP PFNGLLINKPROGRAMARBPROC) (GLhandle programObj);
+typedef void (GL_APIENTRYP PFNGLVALIDATEPROGRAMARBPROC) (GLhandle programObj);
+typedef void (GL_APIENTRYP PFNGLUSEPROGRAMOBJECTARBPROC) (GLhandle programObj);
+typedef GLint (GL_APIENTRYP PFNGLGETUNIFORMLOCATIONARBPROC) (GLhandle programObj, const GLchar*name);
+typedef void (GL_APIENTRYP PFNGLGETACTIVEUNIFORMPROC) (GLuint program, GLuint index, GLsizei bufSize, GLsizei *length, GLint *size, GLenum *type, GLchar *name);
+typedef void (GL_APIENTRYP PFNGLUNIFORM1FARBPROC) (GLint location, GLfloat v0);
+typedef void (GL_APIENTRYP PFNGLUNIFORM2FARBPROC) (GLint location, GLfloat v0, GLfloat v1);
+typedef void (GL_APIENTRYP PFNGLUNIFORM3FARBPROC) (GLint location, GLfloat v0, GLfloat v1, GLfloat v2);
+typedef void (GL_APIENTRYP PFNGLUNIFORM4FARBPROC) (GLint location, GLfloat v0, GLfloat v1, GLfloat v2, GLfloat v3);
+typedef void (GL_APIENTRYP PFNGLUNIFORM1IARBPROC) (GLint location, GLint v0);
+typedef void (GL_APIENTRYP PFNGLUNIFORM2IARBPROC) (GLint location, GLint v0, GLint v1);
+typedef void (GL_APIENTRYP PFNGLUNIFORM3IARBPROC) (GLint location, GLint v0, GLint v1, GLint v2);
+typedef void (GL_APIENTRYP PFNGLUNIFORM4IARBPROC) (GLint location, GLint v0, GLint v1, GLint v2, GLint v3);
+typedef void (GL_APIENTRYP PFNGLUNIFORMMATRIX2FVARBPROC) (GLint location, GLsizei count, GLboolean transpose, const GLfloat *value);
+typedef void (GL_APIENTRYP PFNGLUNIFORMMATRIX3FVARBPROC) (GLint location, GLsizei count, GLboolean transpose, const GLfloat *value);
+typedef void (GL_APIENTRYP PFNGLUNIFORMMATRIX4FVARBPROC) (GLint location, GLsizei count, GLboolean transpose, const GLfloat *value);
+typedef void (GL_APIENTRYP PFNGLGETOBJECTPARAMETERFVARBPROC) (GLhandle obj, GLenum pname, GLfloat *params);
+typedef void (GL_APIENTRYP PFNGLGETOBJECTPARAMETERIVARBPROC) (GLhandle obj, GLenum pname, GLint *params);
+
+extern PFNGLCREATESHADEROBJECTARBPROC glCreateShaderObject;
+extern PFNGLDELETEOBJECTARBPROC glDeleteObject;
+extern PFNGLDETACHOBJECTARBPROC glDetachObject;
+extern PFNGLSHADERSOURCEARBPROC glShaderSource;
+extern PFNGLCOMPILESHADERARBPROC glCompileShader;
+extern PFNGLCREATEPROGRAMOBJECTARBPROC glCreateProgramObject;
+extern PFNGLGETPROGRAMIVPROC glGetProgramiv;
+extern PFNGLATTACHOBJECTARBPROC glAttachObject;
+extern PFNGLLINKPROGRAMARBPROC glLinkProgram;
+extern PFNGLVALIDATEPROGRAMARBPROC glValidateProgram;
+extern PFNGLUSEPROGRAMOBJECTARBPROC glUseProgramObject;
+extern PFNGLGETUNIFORMLOCATIONARBPROC glGetUniformLocation;
+extern PFNGLGETACTIVEUNIFORMPROC glGetActiveUniform;
+extern PFNGLUNIFORM1FARBPROC glUniform1f;
+extern PFNGLUNIFORM2FARBPROC glUniform2f;
+extern PFNGLUNIFORM3FARBPROC glUniform3f;
+extern PFNGLUNIFORM4FARBPROC glUniform4f;
+extern PFNGLUNIFORM1IARBPROC glUniform1i;
+extern PFNGLUNIFORM2IARBPROC glUniform2i;
+extern PFNGLUNIFORM3IARBPROC glUniform3i;
+extern PFNGLUNIFORM4IARBPROC glUniform4i;
+extern PFNGLUNIFORMMATRIX2FVARBPROC glUniformMatrix2fv;
+extern PFNGLUNIFORMMATRIX3FVARBPROC glUniformMatrix3fv;
+extern PFNGLUNIFORMMATRIX4FVARBPROC glUniformMatrix4fv;
+extern PFNGLGETOBJECTPARAMETERFVARBPROC glGetObjectParameterfv;
+extern PFNGLGETOBJECTPARAMETERIVARBPROC glGetObjectParameteriv;
+
+// ARB_vertex_shader
+typedef void (GL_APIENTRYP PFNGLDISABLEVERTEXATTRIBARRAYPROC) (GLuint index);
+typedef void (GL_APIENTRYP PFNGLENABLEVERTEXATTRIBARRAYPROC) (GLuint index);
+typedef GLint (GL_APIENTRYP PFNGLGETATTRIBLOCATIONPROC) (GLuint program, const GLchar *name);
+typedef void (GL_APIENTRYP PFNGLVERTEXATTRIBPOINTERARBPROC) (GLuint index, GLint size, GLenum type, GLboolean normalized, GLsizei stride, const GLvoid *pointer);
+
+extern PFNGLDISABLEVERTEXATTRIBARRAYPROC glDisableVertexAttribArray;
+extern PFNGLENABLEVERTEXATTRIBARRAYPROC glEnableVertexAttribArray;
+extern PFNGLGETATTRIBLOCATIONPROC glGetAttribLocation;
+extern PFNGLVERTEXATTRIBPOINTERARBPROC glVertexAttribPointer;
+
+// ARB_fragment_shader
+
+// ARB_vertex_array_object
+#define GL_VERTEX_ARRAY_BINDING     0x85B5
+#define GL_WRITE_ONLY               0x88B9
+#define GL_ELEMENT_ARRAY_BUFFER_ARB 0x8893
+
+typedef void (GL_APIENTRYP PFNGLBINDVERTEXARRAYARBPROC) (GLuint array);
+typedef void (GL_APIENTRYP PFNGLDELETEVERTEXARRAYSARBPROC) (GLsizei n, const GLuint *arrays);
+typedef void (GL_APIENTRYP PFNGLGENVERTEXARRAYSARBPROC) (GLsizei n, GLuint *arrays);
+typedef GLboolean (GL_APIENTRYP PFNGLISVERTEXARRAYARBPROC) (GLuint array);
+
+extern PFNGLBINDVERTEXARRAYARBPROC glBindVertexArray;
+extern PFNGLDELETEVERTEXARRAYSARBPROC glDeleteVertexArrays;
+extern PFNGLGENVERTEXARRAYSARBPROC glGenVertexArrays;
+extern PFNGLISVERTEXARRAYARBPROC glIsVertexArray;
+
+// GL_OES_element_index_uint
+#define GL_UNSIGNED_INT 0x1405
+
+// OpenGL 1.2
+#define GL_UNSIGNED_SHORT_4_4_4_4 0x8033
+#define GL_UNSIGNED_SHORT_5_5_5_1 0x8034
+#define GL_UNSIGNED_SHORT_5_6_5   0x8363
+
+// OpenGL 1.3
+#define GL_MAX_TEXTURE_UNITS 0x84E2
+
+// OpenGL 1.5
+#define GL_ARRAY_BUFFER         0x8892
+#define GL_ELEMENT_ARRAY_BUFFER 0x8893
+#define GL_STATIC_DRAW          0x88E4
+#define GL_DYNAMIC_DRAW         0x88E8
+
+// OpenGL 2.0
+typedef void (GL_APIENTRYP PFNGLGETSHADERINFOLOGPROC) (GLuint shader, GLsizei bufSize, GLsizei *length, GLchar *infoLog);
+typedef void (GL_APIENTRYP PFNGLGETPROGRAMINFOLOGPROC) (GLuint program, GLsizei bufSize, GLsizei *length, GLchar *infoLog);
+
+extern PFNGLGETSHADERINFOLOGPROC glGetShaderInfoLog;
+extern PFNGLGETPROGRAMINFOLOGPROC glGetProgramInfoLog;
+
+//--------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+#endif // EGE_AIRPLAY_GRAPHICS_OPENGL_EXTENSIONSOGL_H
