e6f35c971d465f052a121cabcac66e814b3a2cf8
enmaniac <enmaniac@1e37b749-e30f-da40-b4df-3e99ea2a9c57>
2013-03-18 21:57:33 +0000

    IOS implementation introduction changes
    git-svn-id: svn://localhost/EGE@1485 1e37b749-e30f-da40-b4df-3e99ea2a9c57

---
+++ trunk/Sources/iOS/String/StringUtilsIOS.cpp
@@ -0,0 +1,50 @@
+#include "Core/String/StringUtils.h"
+#include <stdlib.h>
+
+EGE_NAMESPACE_BEGIN
+
+// TAGE - HACK for Airplay where strtoll function seems to be missing for VC compiler
+#ifdef _MSC_VER
+extern "C"
+{
+  int64_t _strtoi64(const char *nptr, char **endptr, int base);
+  int64_t strtoll(const char *nptr, char **endptr, int base)
+  {
+      return _strtoi64(nptr, endptr, base);
+  }
+}
+#endif // _MSC_VER
+
+//--------------------------------------------------------------------------------------------------------------------------------------------------------------
+s32 StringUtils::ToInt(const char* text, bool* error)
+{
+  char* end;
+
+  s32 value = strtol(text, &end, 10);
+
+  // check for error
+  if (error && (0 != *end))
+  {
+    *error = true;
+  }
+
+  return value;
+}
+//--------------------------------------------------------------------------------------------------------------------------------------------------------------
+s64 StringUtils::ToInt64(const char* text, bool* error)
+{
+  char* end;
+
+  s64 value = strtoll(text, &end, 10);
+
+  // check for error
+  if (error && (0 != *end))
+  {
+    *error = true;
+  }
+
+  return value;
+}
+//--------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+EGE_NAMESPACE_END
