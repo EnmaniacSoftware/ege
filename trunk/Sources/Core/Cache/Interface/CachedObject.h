edd3bc4862ead2f0070b79a22f1d4224d5fd394e
enmaniac <enmaniac@1e37b749-e30f-da40-b4df-3e99ea2a9c57>
2014-01-24 20:53:09 +0000

    Cached Object introduced
    git-svn-id: svn://localhost/EGE@1751 1e37b749-e30f-da40-b4df-3e99ea2a9c57

---
+++ trunk/Sources/Core/Cache/Interface/CachedObject.h
@@ -0,0 +1,86 @@
+#ifndef EGE_CORE_CACHE_CACHEDOBJECT_H
+#define EGE_CORE_CACHE_CACHEDOBJECT_H
+
+/*! Cached objects are wrappers around real data types with additional validity information. 
+ */
+
+#include "EGE.h"
+
+EGE_NAMESPACE_BEGIN
+
+//--------------------------------------------------------------------------------------------------------------------------------------------------------------
+template <typename T>
+class CachedObject
+{
+  public:
+
+    CachedObject();
+    CachedObject(T& data);
+    CachedObject(const CachedObject<T>& other);
+
+    const CachedObject& operator = (const T& data);
+
+    /*! Returns TRUE if data is valid. */
+    bool isValid() const;
+    /*! Invalidates data. */
+    void invalidate();
+
+    /*! Returns value. */
+    const T& data() const;
+
+  private:
+
+    /*! Cached data. */
+    T m_data;
+    /*! Validity flag. */
+    bool m_valid;
+};
+//--------------------------------------------------------------------------------------------------------------------------------------------------------------
+template <typename T>
+CachedObject<T>::CachedObject() : m_valid(false)
+{
+}
+//--------------------------------------------------------------------------------------------------------------------------------------------------------------
+template <typename T>
+CachedObject<T>::CachedObject(T& data) : m_data(data),
+                                         m_valid(true)
+{
+}
+//--------------------------------------------------------------------------------------------------------------------------------------------------------------
+template <typename T>
+CachedObject<T>::CachedObject(const CachedObject<T>& other) : m_data(other.m_data),
+                                                              m_valid(other.m_valid)
+{
+}
+//--------------------------------------------------------------------------------------------------------------------------------------------------------------
+template <typename T>
+const CachedObject<T>& CachedObject<T>::operator = (const T& data)
+{
+  m_data  = data;
+  m_valid = true;
+
+  return *this;
+}
+//--------------------------------------------------------------------------------------------------------------------------------------------------------------
+template <typename T>
+bool CachedObject<T>::isValid() const
+{
+  return m_valid;
+}
+//--------------------------------------------------------------------------------------------------------------------------------------------------------------
+template <typename T>
+void CachedObject<T>::invalidate()
+{
+  m_valid = false;
+}
+//--------------------------------------------------------------------------------------------------------------------------------------------------------------
+template <typename T>
+const T& CachedObject<T>::data() const
+{
+  return m_data;
+}
+//--------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+EGE_NAMESPACE_END
+
+#endif // EGE_CORE_CACHE_CACHEDOBJECT_H
---
+++ trunk/Sources/EGECachedObject.h
@@ -0,0 +1,15 @@
+#ifndef EGE_CACHEDOBJECT_H
+#define EGE_CACHEDOBJECT_H
+
+#include "Core/Platform.h"
+#include "Core/Cache/Interface/CachedObject.h"
+
+EGE_NAMESPACE_BEGIN
+
+//--------------------------------------------------------------------------------------------------------------------------------------------------------------
+typedef CachedObject<bool> CachedBool;
+//--------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+EGE_NAMESPACE_END
+
+#endif // EGE_CACHEDOBJECT_H
