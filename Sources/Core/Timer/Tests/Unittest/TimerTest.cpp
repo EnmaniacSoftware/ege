ca2e96c8b4b79691c1bc99878e4783319f1b3a0c
Albert Banaszkiewicz <enmaniac@o2.pl>
2014-04-15 21:31:39 +0200

    [EGE #33] Unittests added.

---
+++ Sources/Core/Timer/Tests/Unittest/TimerTest.cpp
@@ -0,0 +1,84 @@
+#include "TestFramework/Interface/TestBase.h"
+#include <EGETimer.h>
+#include <windows.h>
+
+EGE_NAMESPACE
+
+//--------------------------------------------------------------------------------------------------------------------------------------------------------------
+class TimerTest : public TestBase
+{
+  protected:
+
+    static void SetUpTestCase();
+    static void TearDownTestCase();
+
+  protected:
+
+    virtual void SetUp();
+    virtual void TearDown();
+
+  protected:
+
+    /*! Methods sleeping for a given interval. 
+     *  @param  miliseconds Sleep interval.
+     */
+    void osSleep(u32 miliseconds);
+};
+//--------------------------------------------------------------------------------------------------------------------------------------------------------------
+void TimerTest::SetUpTestCase()
+{
+}
+//--------------------------------------------------------------------------------------------------------------------------------------------------------------
+void TimerTest::TearDownTestCase()
+{
+}
+//--------------------------------------------------------------------------------------------------------------------------------------------------------------
+void TimerTest::SetUp()
+{
+}
+//--------------------------------------------------------------------------------------------------------------------------------------------------------------
+void TimerTest::TearDown()
+{
+}
+//--------------------------------------------------------------------------------------------------------------------------------------------------------------
+void TimerTest::osSleep(u32 miliseconds)
+{
+  Sleep(miliseconds);
+}
+//--------------------------------------------------------------------------------------------------------------------------------------------------------------
+TEST_F(TimerTest, GetMiliseconds)
+{
+  const u32 KSleepIntervalMs          = 100;
+  const u32 KSleepIntervalMsDeviation = 2;
+
+  // register time stamp
+  s64 stamp = Timer::GetMiliseconds();
+
+  // sleep using OS functionality
+  osSleep(KSleepIntervalMs);
+
+  // calculate time difference
+  stamp = Timer::GetMiliseconds() - stamp;
+
+  // validate
+  EXPECT_TRUE(((KSleepIntervalMs - KSleepIntervalMsDeviation) <= stamp) && ((KSleepIntervalMs + KSleepIntervalMsDeviation) >= stamp));
+}
+//--------------------------------------------------------------------------------------------------------------------------------------------------------------
+TEST_F(TimerTest, GetMicroseconds)
+{
+  const u32 KSleepIntervalMs          = 100;
+  const u32 KSleepIntervalMsDeviation = 2;
+
+  // register time stamp
+  s64 stamp = Timer::GetMicroseconds();
+
+  // sleep using OS functionality
+  osSleep(KSleepIntervalMs);
+
+  // calculate time difference
+  stamp = Timer::GetMicroseconds() - stamp;
+
+  // validate
+  EXPECT_TRUE((((KSleepIntervalMs - KSleepIntervalMsDeviation) * 1000) <= stamp) && (((KSleepIntervalMs + KSleepIntervalMsDeviation) * 1000) >= stamp));
+}
+//--------------------------------------------------------------------------------------------------------------------------------------------------------------
