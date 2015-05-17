eca9feaa9c42850f959f48f45baad67752ecae31
Albert Banaszkiewicz <enmaniac@o2.pl>
2015-05-17 13:46:19 +0200

    [EGE #127] iOS implementation.

--- Sources/iOS/Atomic/Implementation/AtomicOperationsIOS.cpp
+++ Sources/iOS/Atomic/Implementation/AtomicOperationsIOS.cpp
@@ -24,5 +24,28 @@ s32 egeAtomicCompareAndSet(volatile s32& value, s32 compareValue, s32 newValue)
   return OSAtomicCompareAndSwap32Barrier(compareValue, newValue, reinterpret_cast<volatile int32_t*>(&value));
 }
 //--------------------------------------------------------------------------------------------------------------------------------------------------------------
+void* egeAtomicSet(void*& value, void* newValue)
+{
+  void* oldValue = value;
+  
+ 	while (true)
+	{
+		if (OSAtomicCompareAndSwapPtrBarrier(oldValue, newValue, reinterpret_cast<void* volatile *>(&value)))
+    {
+      break;
+    }
+
+    oldValue = value;
+  }
+  
+  return oldValue;
+}
+//--------------------------------------------------------------------------------------------------------------------------------------------------------------
+void* egeAtomicCompareAndSet(void*& value, void* compareValue, void* newValue)
+{
+  (void) OSAtomicCompareAndSwapPtrBarrier(compareValue, newValue, reinterpret_cast<void* volatile *>(&value));
+  return compareValue;
+}
+//--------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 EGE_NAMESPACE_END
